a29c1f78a2ff72ba81b9c27734a5b50d93786842
MD RUBEL <hello@mdrubel.net>
2022-05-30 22:31:05 +0600

    Initial Commit

---
+++ Moviella/MoviellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviellaApp.swift
+//  Moviella
+//
+//  Created by MD RUBEL on 30/5/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
